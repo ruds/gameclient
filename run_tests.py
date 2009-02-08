bc55e7a5542133413f953083413808c37086689b
sneaky.ruds <sneaky.ruds@2df9dbf0-eb5a-11dd-b00f-9fabb4b2ff13>
2009-02-08 03:11:20 +0000

    Fixed bug in import.

--- run_tests.py
+++ run_tests.py
@@ -2,7 +2,7 @@
 
 from gameclient.cards.cardlib_test import *
 from gameclient.cards.bridge.rules_test import *
-from gameclient.util import *
+from gameclient.util_test import *
 
 import unittest
 
